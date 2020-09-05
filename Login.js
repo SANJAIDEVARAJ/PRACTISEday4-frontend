55dbcd706ed803d705de8d00ad973283c6e2b0f9
SANJAIDEVARAJ <67061462+SANJAIDEVARAJ@users.noreply.github.com>
2020-09-05 19:31:48 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -3,58 +3,44 @@ import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
 
-
-
-
 class Login extends React.Component {
-
     constructor(props){
         super();
         this.state={
             email:"",password:""
         }
-     }
- 
-
-
-	 name=(event)=>{
- 
-        let target = event.target;
-
-        if(target.name === "email"){
-            console.log(target.value)
-            this.setState({
+	 }
+	 
+name=(event)=>{
+  let target = event.target;
+ if(target.name === "email"){
+        this.setState({
                 email : target.value
             })
         }
         else if(target.name === "password"){
-            console.log(target.value)
+            
             this.setState({
                 password : target.value
             })}
-        
-
 }
 
-
 handler = (e) =>{
-	e.preventDefault()
-	console.log("hello")
+	e.preventDefault()	
 let data = {
     "email":this.state.email,
     "password":this.state.password
 }   
-axios.post("http://localhost:4000/login",data)
-.then((res)=>
-this.props.history.push("/dashboard"))
-.catch((err)=>console.log("error"))
 
+axios.post("https://day4backend.herokuapp.com/login",data)
+.then((res)=>{
+	console.log(this.props);
+	alert("Login successful");	
+    this.props.history.push("/dashboard")})
+.catch((err)=>console.log("error"))
 }
 
-
-
-render(){
-	console.log(this.props)
+render(){	
     return(
   <div>
 <div class="container">
@@ -89,19 +75,13 @@ render(){
 			</div>
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
-					Don't have an account?  <Link to="/register" >Sign Up	
-								</Link>
-                                
+					Don't have an account?  <Link to="/register" >Sign Up</Link> 				           
 				</div>
-				
 			</div>
 		</div>
 	</div>
 </div>
-
   </div> );
 }
-
 }
-
 export default Login;
